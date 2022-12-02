d71e97566f6ba0e6e150cec23ddb5d478925915a
user <user@WIN-349MCH6R7UR>
2022-12-02 20:36:41 +0300

    Добавьте файлы проекта.

---
+++ 2.1.1_Zadanie_YP/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _2._1._1_Zadanie_YP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                double x = 0;
+                double y = 0;
+
+                double r1 = 15;
+                double r2 = 25;
+
+                x = double.Parse(Console.ReadLine());
+                try
+                {
+                    int result = Convert.ToInt16(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Это не число ");
+                }
+
+                y = double.Parse(Console.ReadLine());
+
+                if (x * x + y * y == r1 * r1)
+                {
+                    Console.WriteLine("На границе");
+                }
+                else if (x * x + y * y < r1 * r1)
+                {
+                    Console.WriteLine("Внутри");
+                }
+                else if (x * x + y * y == r2 * r2)
+                {
+                    Console.WriteLine("На границе");
+                }
+                else if (x * x + y * y < r2 * r2)
+                {
+                    Console.WriteLine("Вне");
+                }
+                else Console.WriteLine("Внутри");
+            }
+        }
+
+    }
+}
+
---
+++ Zadanie 3 YP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Zadanie_3_YP
+{
+    class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите курс доллара: ");
+            double rate = double.Parse(Console.ReadLine());
+            Console.WriteLine("\nfor\n");
+            for (int i = 5; i <= 120; i += 5)
+                Console.WriteLine("{0} долларов - {1} рублей", i, i * rate);
+            Console.WriteLine("\nwhile\n");
+            int j = 5;
+            while (j <= 120)
+            {
+                Console.WriteLine("{0} долларов - {1} рублей", j, j * rate);
+                j += 5;
+            }
+            Console.WriteLine("\ndo while\n");
+            int k = 0;
+            do
+            {
+                k += 5;
+                Console.WriteLine("{0} долларов - {1} рублей", k, k * rate);
+            }
+            while (k < 120);
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
